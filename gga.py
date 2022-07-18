a68a54119d0cde3f7d9a326fc08764e3d8052a7f
Collin Farquhar <collinfarquhar@Collins-MacBook-Pro.local>
2022-07-18 19:08:35 +0100

    code back up. Things are running

--- gga.py
+++ gga.py
@@ -49,12 +49,13 @@ class GGA():
         self.geo = geo
         self.n_initial_random_samples = 100
         self.epsilon = 1
-        self.epsilon_decay = 0.99
+        self.epsilon_decay = 0.96
         self.epsilon_min = 0.05
         self.n_optimze_steps = 100
         self.n_samples_to_generate = 100
         self.n_selected_samples = 10
-        self.batch_size = 64
+        self.batch_size = 32
+        self.n_steps = 100
 
         self.samples = [] 
         self.costs = []
@@ -67,16 +68,20 @@ class GGA():
         samples = self.generate_random_samples(self.batch_size)
         costs = self.evaluate_costs(samples)
         probs = self.evaluate_probabilities(costs)
-        self.samples += samples
+
+        #print("samples", samples)
+
+        #self.samples += samples # seems to be buggged
+        for sample in samples:
+            self.samples.append(sample)
         self.costs += costs
-        print(type(probs))
         self.probs += list(probs)
-        # print(samples)
-        # print(costs)
-        # print(probs)
-        for i, cost in enumerate(costs):
-            print("cost:", cost, "softmax:", softmax(cost))
-            print("cost:", cost, "prob:", self.probs[i])
+
+        # print("self.samples", self.samples)
+        # print("self.costs", self.costs)
+        # print("self.probs", self.probs)
+
+        self.train()
 
     def generate_random_samples(self, n_samples=1):
         return np.random.uniform(low=self.bounds[0], high=self.bounds[1], 
@@ -89,27 +94,39 @@ class GGA():
         # One option would be to use the scipy minimize function, which can be 
         # passed a callback function that is called on every step.
 
-        samples_and_costs = [] # Best point from each in optimization trajectory
+        samples = [] 
+        costs = []
         for starting_point in starting_points:
             result = least_squares(objective_function, starting_point, bounds=bounds, 
                                    ftol=1e-15, xtol=None, gtol=None, max_nfev=1e3) # max_nfev could potentially be set to None
-            samples_and_costs.append([result.x, result.cost]) 
-        return samples_and_costs
+            samples.append(result.x) 
+            costs.append(result.cost)
+        return samples, costs 
 
     def generate(self):
         """ Generate samples, a subset of which can be selected to be passed to the optimizer.
         Generate samples both randomly and using the generator based on the epsilon parameter. """
         n_exploration_samples = ceil(self.epsilon * self.n_samples_to_generate)
         n_samples_with_generator = self.n_samples_to_generate - n_exploration_samples
-        generated_samples = []
 
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+            if self.epsilon < self.epsilon_min:
+                self.epsilon = self.epsilon_min
+
+        generated_samples = []
         # Exploration: choose some number of samples randomly
-        generated_samples += self.generate_random_samples(n_exploration_samples) 
+        random_samples = self.generate_random_samples(n_exploration_samples) 
+
+        for sample in random_samples:
+            generated_samples.append(sample)
 
         # Generate samples with the generative model
-        generated_samples += self.geo._generator.generate_samples(n_samples_with_generator)
+        if n_samples_with_generator > 0:
+            generator_samples = self.geo._generator.generate_samples(n_samples_with_generator)
+            for sample in generator_samples:
+                generated_samples.append(sample)
 
-        # Update history?
         return generated_samples
     
     def evaluate_costs(self, samples):
@@ -122,7 +139,8 @@ class GGA():
         """ Can have more general selection strategies later, but for now, 
         can just pick the generated samples with the best cost. """
         best_costs_indices = np.argsort(costs)[:self.n_selected_samples]
-        return [(samples[idx], costs[idx]) for idx in best_costs_indices]
+        #return [(samples[idx], costs[idx]) for idx in best_costs_indices]
+        return [samples[idx] for idx in best_costs_indices]
 
     def train(self):
         """ train the generator after getting new sample and cost data from the optimizer and
@@ -134,17 +152,21 @@ class GGA():
         n_previous_samples = len(self.samples) - self.n_selected_samples 
         most_recent_samples_indices = list( range(n_previous_samples, len(self.samples)) )
         past_indices = list(range(n_previous_samples))
-        print("n_prev", n_previous_samples)
-        print("lpi", len(past_indices))
-        print("bs", self.batch_size)
-        print("n_selected", self.n_selected_samples)
+        # print("n_prev", n_previous_samples)
+        # print("lpi", len(past_indices))
+        # print("bs", self.batch_size)
+        # print("n_selected", self.n_selected_samples)
         batch_indices = most_recent_samples_indices + random.sample(past_indices, self.batch_size - self.n_selected_samples)
         random.shuffle(batch_indices)
+        # print("\n\nbatch indices", batch_indices)
+        # print("\n\nlen samples", len(self.samples), self.samples)
         batch = np.array([self.samples[i] for i in batch_indices])
         probs = np.array([self.probs[i] for i in batch_indices])
         
+        # print("\n\nbatch size", batch.size, "\n\n")
+        # print(batch)
         self.geo._generator.train(
-            n_epochs=1,
+            n_epochs=10,
             xtrain=batch,
             probs=probs,
             batch_size=None,
@@ -154,7 +176,45 @@ class GGA():
         )
 
     def run(self):
-        pass
+        for i in range(self.n_steps):
+            # ------------------> Generation <------------------
+            print("Generation")
+            # generate
+            new_samples = self.generate()
+            # evaluate  
+            print("Evaluate")
+            new_costs = self.evaluate_costs(new_samples)
+
+            # ------------------> Selection <------------------
+            print("Selection")
+            samples_to_optmize = self.select_samples_for_optimizer(new_samples, new_costs)
+            # -----------------------------------------------
+
+            # ------------------> Optimize <------------------
+            print("Optimize")
+            optimized_points, optimized_costs = self.optimize(samples_to_optmize)
+            print("optimized costs:", optimized_costs)
+            # -----------------------------------------------
+
+            # ------------------> Save <------------------
+            print("Save")
+            for point in optimized_points:
+                new_samples.append(point)
+
+            new_costs += optimized_costs
+            new_probs = self.evaluate_probabilities(new_costs)
+
+            for sample in new_samples:
+                self.samples.append(sample)
+            self.costs += new_costs
+            self.probs += list(new_probs) 
+            # -----------------------------------------------
+
+            # ------------------> Train <------------------
+            print("Train")
+            self.train()
+            # ---------------------------------------
+
 
 if __name__ == "__main__":
     # Test with optimizer
@@ -182,7 +242,7 @@ if __name__ == "__main__":
     # -------------------------------------------------------------
 
 
-    # ---------------> Test GGA generate function :  <------------------
+    # ---------------> Test GGA run function :  <------------------
     generator = ContinuousVAEGenerator(
             sample_size=n_params,
             encoder_widths=[50, 20],
@@ -192,9 +252,11 @@ if __name__ == "__main__":
         )
     #print(generator.generate(1))
     initial_bitstrings = np.random.rand(1, n_params)
-    print("initial_bitstring", initial_bitstrings)
+    #initial_bitstrings = GGA.generate_random_samples(1)
+    #print("initial_bitstring", initial_bitstrings)
     geo = QEOStandalone(objective=quadratic, generator=generator, bitstrings=initial_bitstrings) 
     gga = GGA(objective_function=quadratic, n_parameters=n_params, bounds=bounds, geo=geo) 
-    gga.train()
-    res = gga.generate()
-    print(res)
+    #gga.train()
+    gga.run()
+    # res = gga.generate()
+    # print(res)
